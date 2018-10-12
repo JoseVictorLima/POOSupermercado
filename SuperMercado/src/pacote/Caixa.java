7021f5ba0d73be86bc2f7ac4f596e77ed12ec87e
JoseVictorLima <jsvctr123@gmail.com>
2018-10-12 11:54:41 -0300

    Adição de algumas variaveis e metodos em caixa

--- SuperMercado/src/pacote/Caixa.java
+++ SuperMercado/src/pacote/Caixa.java
@@ -1,5 +1,17 @@
 package pacote;
+import java.util.ArrayList;
 
 public class Caixa {
-
+	private int id;
+	private float valorNoCaixa;
+	private ArrayList<String> historico = new ArrayList();
+	
+	
+	public void Entrar() {
+		System.out.println("Entrar Caixa N" + this.id);
+	}
+	
+	public void Sair() {
+		System.out.println("Sair Caixa N" + this.id);
+	}
 }
